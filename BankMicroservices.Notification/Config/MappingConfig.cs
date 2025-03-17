b3eb8cf5b4cd4311be77956655911d4310a7e2db
Cesar Campagnucci Junior <kaissar_jr@hotmail.com>
2025-03-17 11:48:39 -0300

    [FEATURE] - Adding Notification Service

---
+++ BankMicroservices.Notification/Config/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BankMicroservices.Notification.Data.ValueObjects;
+using BankMicroservices.Notification.Model;
+
+namespace BankMicroservices.Notification.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<NotificationVO, NotificationModel>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ BankMicroservices.Notification/Controllers/NotificationController.cs
@@ -0,0 +1,42 @@
+﻿using BankMicroservices.Notification.Data.ValueObjects;
+using BankMicroservices.Notification.Repository;
+using BankMicroservices.Notification.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankMicroservices.Notification.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private INotificationRepository _repository;
+
+        public NotificationController(INotificationRepository repository)
+        {
+            _repository = repository ?? throw new
+                ArgumentNullException(nameof(repository));
+        }
+
+        [HttpPatch("{id}")]
+        [Authorize]
+        public async Task<ActionResult> SetAsRead(long id)
+        {
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+            bool done = await _repository.SetAsRead(id, userId);
+            return done? Ok() : BadRequest();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = Role.Admin)]
+        public async Task<ActionResult<IEnumerable<NotificationVO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+    }
+}
